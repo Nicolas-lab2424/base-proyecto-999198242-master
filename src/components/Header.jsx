b956498c79891c1003eb9c68c20b97cefa43ba72
Nicolas-lab2424 <Conigaleano4@gmail.com>
2025-08-12 14:45:47 -0300

    fix:logre arreglar las baras de navegacion y le puse menu hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,49 +9,70 @@ const Header = () => {
   }
 
   return (
-    <header style={{ backgroundColor: "lightblue" }}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="imagen de logo" />
-      <nav>
+    <header>
+      {/* barra  */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
 
-        <ul>
-          <li><Link to="/" className="bg-primary">Inicio</Link></li>
-          {/* Cambiar elementos a por componentes Link de react-router-dom */}
-          {
-            user && <>
+          {/* Logo */}
+          <Link className="navbar-brand" to="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png"
+              alt="logo"
+              height="40"
+            />
+          </Link>
 
-              <li><Link to="/dashboard">Dashboard</Link></li>
-              <button onClick={handleLogout}>Cerrar sesión</button>
-            </>
-          }
-          {
-            !user && <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/registrate">Registrate</Link></li>
-            </>
-          }
-        </ul>
-
-      </nav>
-
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          {/* menu hamburguesa*/}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </div>
+
+          {/* Links */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Inicio</Link>
+              </li>
+
+              {user && (
+                <>
+                  <li className="nav-item">
+                    <Link to="/dashboard" className="nav-link">Dashboard</Link>
+                  </li>
+                  <li className="nav-item">
+                    <button
+                      onClick={handleLogout}
+                      className="btn btn-outline-danger ms-2"
+                    >
+                      Cerrar sesión
+                    </button>
+                  </li>
+                </>
+              )}
+
+              {!user && (
+                <>
+                  <li className="nav-item">
+                    <Link to="/login" className="nav-link">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/registrate" className="nav-link">Registrate</Link>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
         </div>
       </nav>
-
-
-
     </header>
   )
 }
