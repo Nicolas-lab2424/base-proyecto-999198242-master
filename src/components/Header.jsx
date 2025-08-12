db01b2aa0ea5f2792a63136d93b780876db1d0ce
Nicolas-lab2424 <Conigaleano4@gmail.com>
2025-08-12 14:28:23 -0300

    Add:bootstrap al proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,11 +12,13 @@ const Header = () => {
     <header style={{ backgroundColor: "lightblue" }}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="imagen de logo" />
       <nav>
+
         <ul>
+          <li><Link to="/" className="bg-primary">Inicio</Link></li>
           {/* Cambiar elementos a por componentes Link de react-router-dom */}
           {
             user && <>
-              <li><Link to="/">Inicio</Link></li>
+
               <li><Link to="/dashboard">Dashboard</Link></li>
               <button onClick={handleLogout}>Cerrar sesión</button>
             </>
@@ -28,7 +30,28 @@ const Header = () => {
             </>
           }
         </ul>
+
       </nav>
+
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
+
     </header>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterApp } from './router/RouterApp'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./index.css"
 import { UserProvider } from './context/UserContext'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
